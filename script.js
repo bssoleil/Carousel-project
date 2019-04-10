fa5d73d52ec6072d9b8ffe35c40264def6ea43b5
bssoleil <sadia.bentahar59@gmail.com>
2019-04-10 14:42:08 +0200

    10ieme commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let count = 0;
+let listImages =document.getElementsByClassName("imageCarousel");
+imgShow();
+function imgShow() {
+    if(count > listImages.length - 1) {count = 0}
+    if(count < 0) {count = 4}
+    for (let i = 0; i < listImages.length; i++) {
+        listImages[i].classList.add('displayNone');
+    }
+    listImages[count].classList.remove('displayNone');
+
+}
+
+function imgNext() {
+    count++;
+    imgShow();
+}
+
+function imgPrev() {
+    count--;
+    imgShow();  
+}
+
+
+
